d99d2805980b5a57eb4981a21753bf72eaa109f0
booknap <napasa45@gmaili.com>
2020-11-04 14:12:08 +0700

    add decoration

--- src/components/ForecastCard.vue
+++ src/components/ForecastCard.vue
@@ -1,41 +1,69 @@
 <template>
-  <div class="main-card">
+  <div class="shadow main-card bg-light">
     <div class="date-container">
-      <h1>{{day}}</h1>
-      <h2>{{date}}</h2>
+      <div>{{ day }}</div>
+      <div>{{ date }}</div>
     </div>
     <div class="icon-container">
-      <img src="../assets/sunny.png" class="icon" />
+      <img src="http://openweathermap.org/img/wn/02d.png" class="icon" />
+      <div>{{ weather }}</div>
     </div>
     <div class="temp-container">
-      <h2>day {{dayTemp}}°C</h2>
-      <h2>night {{nightTemp}}°C</h2>
+      <div class="day">
+        <svg
+          width="1em"
+          height="1em"
+          viewBox="0 0 16 16"
+          class="bi bi-brightness-high-fill"
+          fill="#EF810E"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0z" />
+          <path
+            fill-rule="evenodd"
+            d="M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"
+          />
+        </svg>
+        <div>day {{ dayTemp }}°C</div>
+      </div>
+      <div class="night">
+        <svg
+          width="1em"
+          height="1em"
+          viewBox="0 0 16 16"
+          class="bi bi-moon"
+          fill="#fbecb8"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M14.53 10.53a7 7 0 0 1-9.058-9.058A7.003 7.003 0 0 0 8 15a7.002 7.002 0 0 0 6.53-4.47z"
+          />
+        </svg>
+        <div>night {{ nightTemp }}°C</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: [
-    'day',
-    'date',
-    'dayTemp',
-    'nightTemp'
-  ],
+  props: ['day', 'date', 'weather', 'dayTemp', 'nightTemp'],
 };
 </script>
 
-<style scoped>
+<style>
 .main-card {
   display: flex;
   flex-direction: row;
-  width: 1000px;
-  height: 200px;
-  background-color: rgb(189, 189, 189);
-  opacity: 0.5;
+  /* width: 50vw; */
+  height: 5vw;
+  background-color: #707070;
+  /* opacity: 0.5; */
   border-radius: 10px;
-  margin: 40px 0px 40px 0px;
+  margin: 15px 0px 15px 0px;
   transition: transform 0.2s;
+  
 }
 
 .main-card:hover {
@@ -43,17 +71,7 @@ export default {
 }
 
 .icon {
-  height: 50%;
-}
-
-.date-container {
-  width: 40%;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  /* background-color: red; */
+  height: 30%;
 }
 
 h1 {
@@ -61,20 +79,51 @@ h1 {
   vertical-align: middle;
 }
 
-.icon-container {
-  width: 20%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  /* background-color: black; */
-}
-
-.temp-container {
-  width: 40%;
+.date-container {
+  width: 25%;
+  text-align: center;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  /* background-color: blue; */
+}
+
+.icon-container {
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.temp-container {
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin: 1%;
+}
+
+.day {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 50%;
+  align-items: center;
+  justify-content: center;
+  background-color:#E5DE44;
+  border-radius: 10px 0px 0px 10px;
+}
+
+.night {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 50%;
+  align-items: center;
+  justify-content: center;
+  background-color: #053752;
+  border-radius: 0px 10px 10px 0px;
+  color: white;
 }
 </style>
--- src/views/ForecastPage.vue
+++ src/views/ForecastPage.vue
@@ -2,8 +2,14 @@
   <div class="home">
     <Navbar></Navbar>
     <div class="content-container h-100 py-5 d-flex justify-content-center">
-      <div>
-        <ForecastCard day="Monday" date="Nov 3, 2020" dayTemp="25" nightTemp="20"></ForecastCard>
+      <div class="main-content">
+        <ForecastCard day="วันจันทร์" date="3 พย. 2020" weather="มีเมฆเล็กน้อย" dayTemp="25" nightTemp="20"></ForecastCard>
+        <ForecastCard day="วันอังคาร" date="4 พย. 2020" weather="ท้องฟ้าโปร่ง" dayTemp="28" nightTemp="21"></ForecastCard>
+        <ForecastCard day="วันพุธ" date="5 พย. 2020" weather="ท้องฟ้าโปร่ง" dayTemp="28" nightTemp="21"></ForecastCard>
+        <ForecastCard day="วันพฤหัสบดี" date="6 พย. 2020" weather="ท้องฟ้าโปร่ง" dayTemp="28" nightTemp="21"></ForecastCard>
+        <ForecastCard day="วันอังคาร" date="7 พย. 2020" weather="ท้องฟ้าโปร่ง" dayTemp="28" nightTemp="21"></ForecastCard>
+        <ForecastCard day="วันอังคาร" date="8 พย. 2020" weather="ท้องฟ้าโปร่ง" dayTemp="28" nightTemp="21"></ForecastCard>
+        <ForecastCard day="วันอังคาร" date="9 พย. 2020" weather="ท้องฟ้าโปร่ง" dayTemp="28" nightTemp="21"></ForecastCard>
       </div>
     </div>
   </div>
@@ -21,3 +27,12 @@ export default {
   },
 };
 </script>
+
+<style>
+.main-content {
+  width: 50vw;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+}
+</style>
